a5fe9dfbc658ab3a4e69717e2f8b72fe31af9c3c
Angular CLI <angular-cli@angular.io>
2017-09-16 10:45:37 +0000

    chore: initial commit from @angular/cli

---
+++ src/app/animals/animals.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AnimalsRoutingModule } from './animals-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AnimalsRoutingModule
+  ],
+  declarations: []
+})
+export class AnimalsModule { }
